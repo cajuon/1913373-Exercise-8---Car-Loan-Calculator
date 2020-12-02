3994ec88c1227ef138f13aff30affb585ca1fab5
syzwnongithub <syazwan200600@gmail.com>
2020-12-02 13:11:47 +0800

    Intital Commit

---
+++ script.js
@@ -0,0 +1,4 @@
+let totalPrinciple = principal-deposit;
+let totalInterest = (interest/100)*totalPrinciple*years;
+let totalToPay = totalInterest+totalPrinciple;
+let monthlyToPay = totalToPay/years/12;
