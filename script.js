32faad8a8fc67ff5e45f9a4fe62011ec9d35cc28
syzwnongithub <syazwan200600@gmail.com>
2020-12-02 14:39:30 +0800

    Update script

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
-
-let btn = document.getElementById('kira');
-let payment = document.getElementById('payment');
-let principal;
-let deposit;
-let interest;
-let years;
-
-
-btn.addEventListener('click', function(){
-  principal = document.getElementById('principal').value;
-  deposit = document.getElementById('deposit').value;
-  interest = document.getElementById('interest').value;
-  years = document.getElementById('years').value;
-
+//function to calculate monthly payment
+function calcMonthly(principal,deposit,interest,years){
   let totalPrinciple = principal-deposit;
   let totalInterest = (interest/100)*totalPrinciple*years;
   let totalToPay = totalInterest+totalPrinciple;
   let monthlyToPay = totalToPay/years/12;
-  payment.innerHTML = 'RM'+ monthlyToPay.toFixed(2);
+  return monthlyToPay.toFixed(2);
+}
+//declare variable for inputs
+let principal;
+let deposit;
+let interest;
+let years;
+//declare variable for result output
+let payment = document.getElementById('payment');
+
+//declare button
+let btn = document.getElementById('kira');
+//event listener for the button when clicked
+btn.addEventListener('click', function(){
+  //get value from inputs
+  principal = document.getElementById('principal').value;
+  deposit = document.getElementById('deposit').value;
+  interest = document.getElementById('interest').value;
+  years = document.getElementById('years').value;
+  //print the result output
+  payment.innerHTML = 'RM'+ calcMonthly(principal,deposit,interest,years);
   }
 );
